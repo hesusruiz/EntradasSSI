d4879b324eca8656df836129ce2a4895c6322f82
Nil G <nil.guerrero@in2.es>
2019-10-01 14:47:54 +0200

    ADD: Generación claves publicas y privadas, codificadas en base64 + página RequestCredential

---
+++ entradas-ssi-wallet/src/services/KeyGenerator.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from "@angular/core";
+import { Base64 } from 'js-base64';
+
+
+@Injectable()
+export class KeyGeneratorService {
+
+
+    constructor() {
+    }
+
+    generatePublicPrivateKey() {
+        let EC = require('elliptic').ec;
+
+// Create and initialize EC context
+// (better do it once and reuse it)
+        let ec = new EC('secp256k1');
+
+// Generate keys
+        let key = ec.genKeyPair();
+        console.log("KEY---------->",key);
+
+        let pubKey= key.getPublic();
+
+        console.log("Key original --->"+pubKey.toString());
+
+        let encodedKey= Base64.encode(pubKey);
+
+        console.log("Key base64--_>"+encodedKey);
+
+        console.log("Key original decoded -->"+Base64.decode(encodedKey));
+
+        return encodedKey;
+    }
+}
