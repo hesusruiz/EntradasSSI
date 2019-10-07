5cd66e330fbe3ae0a42024b0367689e823923b46
Nil G <nil.guerrero@in2.es>
2019-10-07 18:36:59 +0200

    ADD: JWT of CredentialPresentation with tickedId encrypted in Base64

--- entradas-ssi-wallet/src/pages/show-identity/show-identity.ts
+++ entradas-ssi-wallet/src/pages/show-identity/show-identity.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {User} from "../../models/User";
+import {Base64} from 'js-base64';
+
 
 /**
  * Generated class for the ShowIdentityPage page.
@@ -11,20 +13,61 @@ import {User} from "../../models/User";
 
 @IonicPage()
 @Component({
-  selector: 'page-show-identity',
-  templateUrl: 'show-identity.html',
+    selector: 'page-show-identity',
+    templateUrl: 'show-identity.html',
 })
 export class ShowIdentityPage {
 
-  user: User;
+    user: User;
+    qrData: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.user= navParams.get('user');
+    headerJwt = {
+        "alg": "ES256",
+        "typ": "JWT",
+        "kid": "did:ala:quor:redt:QmeeasCZ9jLbX...ueBJ7d7csxhb#keys-1",
+    };
 
-  }
+    jwtPayload;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShowIdentityPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.user = navParams.get('user');
+
+        let credentials = Base64.encode(this.user.ticketId);
+        console.log('CREDENTIAL: '+credentials);
+
+        this.jwtPayload = {
+            "iss": "did:alastria:quorum:redt:QmeeasCZ9jLbX...ueBJ7d7csxhb",
+            "aud": "did:alastria:quorum:redt:QmeeasCZ9jLbX...ueBJ7d7csxhb",
+            "iat": 1525465044,
+            "vp": {
+                "@context": [
+                    "https://www.w3.org/2018/credentials/v1",
+                    "JWT"
+                ],
+                "type": ["VerifiablePresentation", "AlastriaVPTicket"],
+                "proc": "H398sjHd...kldjUYn475n",
+                // base64url-encoded JWT as string
+                "verifiableCredential": [credentials]
+            }
+        };
+
+        this.qrData=this.generateToken();
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad ShowIdentityPage');
+    }
+
+    generateToken() {
+        // let privatekey = fs.readFileSync("src/PEM/privateKyudo.pem");
+        let jwt = require("jsonwebtoken");
+        let token = jwt.sign(this.jwtPayload, "-----BEGIN EC PRIVATE KEY-----\n" +
+            "MHQCAQEEILI8IeZxN1DQskSvfl1rDnWp/9horl1xAwumWlk0fYejoAcGBSuBBAAK\n" +
+            "oUQDQgAENF5lijsAeVDle1NLoOqt3w0yZ/4VAVBpO3rr6HCOCSDHD+DxirmR0BKW\n" +
+            "YCoGtSiFSUeekSLkIeohUoxoMUTAng==\n" +
+            "-----END EC PRIVATE KEY-----", {header: this.headerJwt, algorithm: "ES256"});
+
+        return token;
+    }
 
 }
