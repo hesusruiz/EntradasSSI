12182d5d22d0e7ade8f39b76da0745fb6ebaaaf1
Nil G <nil.guerrero@in2.es>
2019-10-18 12:53:23 +0200

    FIX: Token sign with correct sign and encrypt

--- entradas-ssi-wallet/src/pages/show-identity/show-identity.ts
+++ entradas-ssi-wallet/src/pages/show-identity/show-identity.ts
@@ -1,102 +1,9 @@
-// import {Component} from '@angular/core';
-// import {IonicPage, NavController, NavParams} from 'ionic-angular';
-// import {User} from "../../models/User";
-// import {Base64} from 'js-base64';
-// import {Activity} from "../tabsPage/activity/activity";
-// import {Storage} from "@ionic/storage";
-//
-//
-// /**
-//  * Generated class for the ShowIdentityPage page.
-//  *
-//  * See https://ionicframework.com/docs/components/#navigation for more info on
-//  * Ionic pages and navigation.
-//  */
-//
-// @IonicPage()
-// @Component({
-//     selector: 'page-show-identity',
-//     templateUrl: 'show-identity.html',
-// })
-// export class ShowIdentityPage {
-//
-//     user: User;
-//     qrData: any;
-//     credentialDecripted;
-//     credentials;
-//     kid:any;
-//     jsontokens = require('jsontokens');
-//
-//     headerJwt = {
-//         "alg": "ES256",
-//         "typ": "JWT",
-//         "kid": this.kid,
-//     };
-//
-//     jwtPayload;
-//
-//     constructor(public navCtrl: NavController, public navParams: NavParams, private localStorage:Storage) {
-//         this.user = navParams.get('user');
-//         this.kid=localStorage.get('kid');
-//         let key= localStorage.get('key');
-//
-//
-//         this.credentials = Base64.encode(this.user.ticketId);
-//         this.credentialDecripted=this.user.ticketId;
-//         console.log('CREDENTIAL: '+this.credentials);
-//
-//         this.jwtPayload = {
-//             "iss": "did:alastria:quorum:redt:QmeeasCZ9jLbX...ueBJ7d7csxhb",
-//             "aud": "did:alastria:quorum:redt:QmeeasCZ9jLbX...ueBJ7d7csxhb",
-//             "iat": 1525465044,
-//             "vp": {
-//                 "@context": [
-//                     "https://www.w3.org/2018/credentials/v1",
-//                     "JWT"
-//                 ],
-//                 "type": ["VerifiablePresentation", "AlastriaVPTicket"],
-//                 "proc": "H398sjHd...kldjUYn475n",
-//                 // base64url-encoded JWT as string
-//                 "verifiableCredential": [this.credentials]
-//             }
-//         };
-//
-//         this.qrData=JSON.stringify(this.generateToken());
-//         console.log(this.qrData);
-//     }
-//
-//     ionViewDidLoad() {
-//         console.log('ionViewDidLoad ShowIdentityPage');
-//     }
-//
-//     generateToken() {
-//         // let privatekey = fs.readFileSync("src/PEM/privateKyudo.pem");
-//         let privatekey=localStorage.getItem('privateKey');
-//         let publickey=localStorage.getItem('privateKey');
-//         //let jwt = require("jsonwebtoken");
-//         //let token = jwt.sign(this.jwtPayload, pk , {header: this.headerJwt, algorithm: "ES256"});
-//
-//         let tokenToSign= new this.jsontokens.TokenSigner('ES256k', privatekey).sign(this.jwtPayload, true, this.headerJwt);
-//         console.log('token to sign: ', tokenToSign);
-//
-//         let verify= new this.jsontokens.TokenVerifier('ES256k', publickey ).verify(tokenToSign);
-//         console.log('is the token verified?: ', verify);
-//
-//         //console.log(token);
-//         return tokenToSign;
-//     }
-//
-//     showIdentity() {
-//         this.navCtrl.push(Activity, {cred:this.credentialDecripted})
-//     }
-//
-// }
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {User} from "../../models/User";
 import {Base64} from 'js-base64';
 import {Activity} from "../tabsPage/activity/activity";
-import {SecureStorage} from "@ionic-native/secure-storage";
+import {Storage} from "@ionic/storage";
 
 
 /**
@@ -117,17 +24,22 @@ export class ShowIdentityPage {
     qrData: any;
     credentialDecripted;
     credentials;
+    kid:any;
+    jsontokens = require('jsontokens');
 
     headerJwt = {
         "alg": "ES256",
         "typ": "JWT",
-        "kid": "did:ala:quor:redt:QmeeasCZ9jLbX...ueBJ7d7csxhb#keys-1",
+        "kid": this.kid,
     };
 
     jwtPayload;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private localStorage:SecureStorage) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private localStorage:Storage) {
         this.user = navParams.get('user');
+        this.kid=localStorage.get('kid');
+        let key= localStorage.get('key');
+
 
         this.credentials = Base64.encode(this.user.ticketId);
         this.credentialDecripted=this.user.ticketId;
@@ -150,6 +62,7 @@ export class ShowIdentityPage {
         };
 
         this.qrData=this.generateToken();
+        console.log(this.qrData);
     }
 
     ionViewDidLoad() {
@@ -158,15 +71,20 @@ export class ShowIdentityPage {
 
     generateToken() {
         // let privatekey = fs.readFileSync("src/PEM/privateKyudo.pem");
-        let jwt = require("jsonwebtoken");
-        let token = jwt.sign(this.jwtPayload, "-----BEGIN EC PRIVATE KEY-----\n" +
-            "MHcCAQEEIOGmCHtuD1qiO1Yv8ZDkFWBgpkd3EuvMTuOEt6v6ZPfSoAoGCCqGSM49\n" +
-            "AwEHoUQDQgAEqAExAPBmINIPQYLy7beBiWsYAJiQyLnTGxraX57Ydkq83GcIWy3u\n" +
-            "Y41PuQR4zUfYln8HGGeyssPRrxEGMSXidQ==\n" +
-            "-----END EC PRIVATE KEY-----", {header: this.headerJwt, algorithm: "ES256"});
+        let privatekey=localStorage.getItem('privateKey');
+        let publickey=localStorage.getItem('publicKey');
+        //let jwt = require("jsonwebtoken");
+        //let token = jwt.sign(this.jwtPayload, pk , {header: this.headerJwt, algorithm: "ES256"});
 
-        console.log(token);
-        return token;
+        let tokenToSign= new this.jsontokens.TokenSigner('ES256k', privatekey).sign(this.jwtPayload, false, this.headerJwt);
+        console.log('token to sign: ', tokenToSign);
+
+        let verify= new this.jsontokens.TokenVerifier('ES256k', publickey ).verify(tokenToSign);
+        console.log('is the token verified?: ', verify);
+
+        //console.log(token);
+        console.log('stringify: ',JSON.stringify(tokenToSign));
+        return JSON.stringify(tokenToSign);
     }
 
     showIdentity() {
